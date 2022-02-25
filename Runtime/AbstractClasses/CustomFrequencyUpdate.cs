ec36a90d7f7ef649704e2b96e48a9c0161ae9b20
AlbertContra <daturafrox@outlook.com>
2022-02-25 16:01:48 -0600

    Added Abstract classes

---
+++ Runtime/AbstractClasses/CustomFrequencyUpdate.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using U.Gears.Math;
+using UnityEngine;
+
+namespace U.Gears
+{
+    public abstract class CustomFrequencyUpdate : MonoBehaviour
+    {
+        public enum TimeModeOptions
+        {
+            DeltaTime,
+            UnscaledDeltaTime,
+        }
+
+        [Header("Frequency")]
+        [SerializeField] private float period = .1f;
+        [SerializeField] private TimeModeOptions timeMode = TimeModeOptions.DeltaTime;
+
+        public float Period { get { return period; } set { period = value; } }
+
+        private float time = 0;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Add the delta time or unscaled time
+            if (timeMode == TimeModeOptions.DeltaTime)
+                time += Time.deltaTime;
+            else
+                time += Time.unscaledDeltaTime;
+
+            // Si se ha completadof
+            if (time >= period)
+            {
+
+                // OncompleteEvent
+                try { CustomUpdate(); } catch (System.Exception e) { Debug.LogError(e); }
+
+                // Restart
+                time -= period.Min(0f);
+            }
+        }
+
+        protected abstract void CustomUpdate();
+
+    }
+}
